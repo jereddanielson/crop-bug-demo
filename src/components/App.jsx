235a6d5c8c664875e48a9e4a13dc5d656aefc07a
Jered <jered@Jereds-MacBook-Pro.local>
2016-07-25 14:10:15 -0700

    Initial Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-var myImage = require("../assets/praisethesun.png"); // just to test that loaders work
+var myImage = require("../assets/praisethesun.png");
+
+import ReactCrop from "react-image-crop";
+require("../../node_modules/react-image-crop/dist/ReactCrop.css");
 
 export default class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				{/* Your app goes in here, this is just boilerplate */}
-				<h1>Hello, World!</h1>
-				<img src={myImage} />
+				<ReactCrop src={myImage} />
+				<button onClick={() => {this.setState({foo: Date.now()})}}>Click me</button>
 			</div>
 		);
 	}
